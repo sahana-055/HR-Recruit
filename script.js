51090cc20cfac7f7adff5717c1ac5d7a8ccc32b5
sahana-055 <sahanac055@gmail.com>
2026-02-17 15:12:35 +0530

    Initial commit: Corporate HR Recruit website

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm")?.addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you for contacting HR Recruit. We will get back to you shortly.");
+});
